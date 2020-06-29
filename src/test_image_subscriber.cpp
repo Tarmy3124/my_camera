0f64b1c0a192a8603f4219227a2504f57f5e09a4
my_camera <1498860484@qq.com>
2020-06-29 20:16:28 +0800

    disparity_success

--- src/test_image_subscriber.cpp
+++ src/test_image_subscriber.cpp
@@ -14,6 +14,32 @@
 //using namespace cv;
 using namespace sensor_msgs;
 using namespace message_filters;
+
+/*void disparity_processor()
+{
+    cv::cvtColor(img_rgb1, img_rgb1, CV_BGR2GRAY);
+    cv::cvtColor(img_rgb2, img_rgb2, CV_BGR2GRAY);
+    //设置参数
+    int numberOfDisparities = 48, SADWindowSize = 11;
+    int uniquenessRatio = 15, speckleWindowSize = 50, speckleRange = 32;
+    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create (0,
+numberOfDisparities, SADWindowSize);
+    int cn = img_rgb1.channels();
+    sgbm->setP1(8 * cn * SADWindowSize * SADWindowSize);
+    sgbm->setP2(32 * cn * SADWindowSize * SADWindowSize);
+    sgbm->setPreFilterCap(63);
+    sgbm->setUniquenessRatio(uniquenessRatio);
+    sgbm->setSpeckleWindowSize(speckleWindowSize);
+    sgbm->setSpeckleRange(speckleRange);
+    sgbm->setDisp12MaxDiff(1);
+//计算并显示视差
+    Mat disp;
+    sgbm->compute(img_rgb1, img_rgb2, disp);
+    disp.convertTo(disp, CV_8U, 255 / (numberOfDisparities*16.));   //将16位符号整形的视差矩阵转换为8位无符号整形矩阵
+    cv::imshow("disparity", disp);
+    //cv::waitKey();
+}*/
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg1,
                    const sensor_msgs::ImageConstPtr& msg2)
 
@@ -34,11 +60,33 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg1,
   cv::Mat img_rgb1, img_rgb2;  
   img_rgb1 = cv_ptr1->image; 
   img_rgb2 = cv_ptr2->image; 
-
+   
   cv::imshow("cam0", img_rgb1); 
   cv::imshow("cam1", img_rgb2); 
+
   // fail if don't have waitKey(3).
-  cv::waitKey(3);
+   // cv::cvtColor(img_rgb1, img_rgb1, cv::COLOR_RGB2GRAY);
+   // cv::cvtColor(img_rgb2, img_rgb2, cv::COLOR_RGB2GRAY);
+//ROS_INFO("It's all right at the moment");
+    //设置参数
+    int numberOfDisparities = 48, SADWindowSize = 11;
+    int uniquenessRatio = 15, speckleWindowSize = 50, speckleRange = 32;
+    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create (0,
+numberOfDisparities, SADWindowSize);
+    int cn = img_rgb1.channels();
+    sgbm->setP1(8 * cn * SADWindowSize * SADWindowSize);
+    sgbm->setP2(32 * cn * SADWindowSize * SADWindowSize);
+    sgbm->setPreFilterCap(63);
+    sgbm->setUniquenessRatio(uniquenessRatio);
+    sgbm->setSpeckleWindowSize(speckleWindowSize);
+    sgbm->setSpeckleRange(speckleRange);
+    sgbm->setDisp12MaxDiff(1);
+//计算并显示视差
+    cv::Mat disp;
+    sgbm->compute(img_rgb1, img_rgb2, disp);
+    disp.convertTo(disp, CV_8U, 255 / (numberOfDisparities*16.));   //将16位符号整形的视差矩阵转换为8位无符号整形矩阵
+    cv::imshow("disparity", disp);
+  cv::waitKey(5);
 }  
 
 int main(int argc, char **argv)  
